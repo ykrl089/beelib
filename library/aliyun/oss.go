657439d4dada8bd30163be76ed7a058488daf6f7
GuoDi <15665207@qq.com>
2016-11-23 23:28:01 +0800

    修改oss

--- library/aliyun/oss.go
+++ library/aliyun/oss.go
@@ -2,7 +2,7 @@
 * @Author: GuoDi
 * @Date:   2016-04-11 22:13:41
 * @Last Modified by:   GuoDi
-* @Last Modified time: 2016-11-11 01:55:40
+* @Last Modified time: 2016-11-23 23:27:03
  */
 package aliyun
 
@@ -13,27 +13,30 @@ import (
 	"mime/multipart"
 )
 
-const (
-	endpoint        = beego.AppConfig.String("aliyun_endpoint")
-	AccessKeyID     = beego.AppConfig.String("aliyun_access_key_id")
-	AccessKeySecret = beego.AppConfig.String("aliyun_access_key_secret")
-	bucketName      = beego.AppConfig.String("aliyun_oss_bucket_name")
-)
-
 var (
-	client *oss.Client
+	client                                             *oss.Client
+	Endpoint, AccessKeyID, AccessKeySecret, BucketName string
 )
 
 func init() {
-	var err error
-	client, err = oss.New(endpoint, AccessKeyID, AccessKeySecret)
-	if err != nil {
+	Endpoint = beego.AppConfig.String("aliyun_endpoint")
+	AccessKeyID = beego.AppConfig.String("aliyun_access_key_id")
+	AccessKeySecret = beego.AppConfig.String("aliyun_access_key_secret")
+	BucketName = beego.AppConfig.String("aliyun_oss_bucket_name")
+	if err := SetClient(); err != nil {
 		fmt.Println("oss warning:", err.Error())
+	}
+}
+func SetClient() error {
+	if client, err = oss.New(Endpoint, AccessKeyID, AccessKeySecret); err != nil {
+		return err
+	} else {
+		return nil
 	}
 }
 func GetBucket(name string) *oss.Bucket {
 	if name == "" {
-		name = bucketName
+		name = BucketName
 	}
 	bucket, err := client.Bucket(name)
 	if err != nil {
